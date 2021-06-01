c12824019e856f80c57d12143a17b4846daa6e03
EC2 Default User <ec2-user@ip-172-31-26-23.ap-northeast-1.compute.internal>
2021-06-01 05:58:47 +0000

    first june

--- db/schema.rb
+++ db/schema.rb
@@ -28,16 +28,16 @@ ActiveRecord::Schema.define(version: 2021_01_17_084726) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "nickname"
-    t.string "full_name"
-    t.text "text"
-    t.string "image"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
+    t.text "text"
+    t.string "image"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
